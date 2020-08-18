00495c5f94bbe4550f241ba6f660c2235fc97e5a
umairarshadbutt <umair6622@gmail.com>
2020-08-18 20:58:58 +0500

    edit and new page update,error message set

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
     validates :password, :presence => true, :confirmation => true, :length => {:within => 6..40}, :on => :create
+    validates :password, :presence => true, :confirmation => true, :length => {:within => 6..40}, :on => :update
         
 end
