ad37a26f7674636a084404c8c4bafe1b7204fea9
EstebanP <epinczinger@gmail.com>
2020-08-18 13:23:11 -0300

    linters integrated, rubocop errors fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-    validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
-	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
-    validates :password, :presence => true, :confirmation => true, :length => {:within => 6..40}, :on => :create
-    validates :password, :presence => true, :confirmation => true, :length => {:within => 6..40}, :on => :update
-        
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, confirmation: true, length: { within: 6..40 }, on: :create
+  validates :password, presence: true, confirmation: true, length: { within: 6..40 }, on: :update
 end
